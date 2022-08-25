02f7d0121efe185a79e263c2d2a9a87a004327ce
Mariano Farias <marianofarias@live.com.ar>
2022-08-25 20:01:38 -0300

    Change styles RecipeDetail

--- client/src/components/RecipeDetail/RecipeDetail.jsx
+++ client/src/components/RecipeDetail/RecipeDetail.jsx
@@ -21,60 +21,72 @@ export default function RecipeDetail() {
             <Link to='/home'>
                 <button>Home</button>
             </Link>
-            <div className={s.subContainer}>
-            <div className={s.box}>
+            <div className={s.subContainer} >
                 <h3 className={s.mainTitle}>{name}</h3>
-                <img className={s.image}
-                    src={image}
-                    alt="Img Not Available"
-                    width="500px"
-                    
-                />
-                </div>
-                <div className={s.box}>
-                    <h3 className={s.subTitle}>Summary</h3>
-                    <p className={s.info}>{summary && summary.replace(/<[^>]+>/g, "")}</p>
-                </div>
-                
-                <div className={s.box}>
-                    <h3 className={s.subTitle}>Diet Types</h3>
-                    {diets?.map((d) => (
-                        d.name ?
-                        <p className={s.info} key={d}>{d.name}</p>
-                    : <p className={s.info} key={d}>{d}</p>))}
-                </div>
-                <div className={s.box}>
-                    <span id="healthScore" className={s.subTitle}>
-                        Health Level:{" "}
-                        <progress
-                            id="healthScore"
-                            max="100"
-                            value={healthScore}
-                            
-                        />{" "}
-                        {healthScore}/100
-                    </span>
-                </div>
-                <div className={s.box}>
-                    
-                {dish ?
+                <div className={s.topBox} >
                     <div>
-                        <h3 className={s.subTitle}>Dish Types: </h3>
-                        {dish?.map(d => {
-                            return (
-                                <p className={s.info} key={d}>- {d[0].toUpperCase() + d.slice(1)} </p>
-                            )
-                        })}
-                    </div> :
-                    <h4 className={s.subTitle}>This recipe has no dish types.</h4>
-                }
-                    
+
+                        <img className={s.image}
+                            src={image}
+                            alt="Img Not Available"
+                            width="500px"
+
+                        />
+                    </div>
+                    <div>
+
+                        <h3 className={s.subTitle}>Summary</h3>
+                        <p className={s.info}>{summary && summary.replace(/<[^>]+>/g, "")}</p>
+
+                    </div>
                 </div>
-                <div className={s.box}>
+
+
+
+                <div className={s.middleBox}>
+                    <div className={''}>
+                        <h3 className={s.subTitle}>Diet Types</h3>
+                        {diets?.map((d) => (
+                            d.name ?
+                                <p className={s.info} key={d}>{d.name}</p>
+                                : <p className={s.info} key={d}>{d}</p>))}
+                    </div>
+                   
+                    <div className={''}>
+
+                        {dish ?
+                            <div>
+                                <h3 className={s.subTitle}>Dish Types: </h3>
+                                {dish?.map(d => {
+                                    return (
+                                        <p className={s.info} key={d}>- {d[0].toUpperCase() + d.slice(1)} </p>
+                                    )
+                                })}
+                            </div> :
+                            <h4 className={s.subTitle}>This recipe has no dish types.</h4>
+                        }
+
+                    </div>
+                    <div className={''}>
+                        <span id="healthScore" className={s.subTitle}>
+                            Health Level:{" "}
+                            <progress
+                                id="healthScore"
+                                max="100"
+                                value={healthScore}
+
+                            />{" "}
+                            {healthScore}/100
+                        </span>
+                    </div>
+                </div>
+
+
+                <div className={s.bottomBox}>
                     <h3 className={s.subTitle}>Steps</h3>
-                    {Array.isArray(steps) ? steps.map((s) => {
+                    {Array.isArray(steps) ? steps.map((st) => {
                         return (
-                            <p className={s.info} key={s.number}>{s.number}: {s.step}</p>
+                            <p className={s.info} key={st.number}>{st.number}: {st.step}</p>
                         )
                     }) : <p className={s.info}>{steps}</p>}
                 </div>
